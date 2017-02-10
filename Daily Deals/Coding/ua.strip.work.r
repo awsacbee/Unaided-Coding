3a0c9a98574a3bb085e96cdde13fd248d767d473
awsacbee <awelden@sacbee.com>
2017-02-10 15:25:39 -0800

    Delete ua.strip.work.r

--- Daily Deals/Coding/ua.strip.work.r
+++
@@ -1,6 +0,0 @@
-## uarecode function ---------------------------------------------------------
-uar.strip<- function(wordstostrip = NULL) {
-  data             <- tolower(str_replace_all((wordstostrip), "[^[:alnum:]]", ""))
-  Encoding(data) <- "UTF-8"
-  return(iconv(data, "UTF-8", "UTF-8",sub='')) ## replace any non UTF-8 by '' 
-}
